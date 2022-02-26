1382f40ce4f6b3b711371490ebde88c33b3b5e0b
blankbrains <990937964@qq.com>
2022-02-26 15:03:35 +0800

    Master Commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -7,5 +7,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hello Git two");
+        System.out.println("master Git");
     }
 }
