0d5941dc90761aabc91dbb8a6031798ae17f8c1b
blankbrains <990937964@qq.com>
2022-02-26 14:46:32 +0800

    First Commit

---
+++ src/main/java/Git.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @Author:DJL-blankbrains
+ * @Date:2022/2/26 14:45
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
