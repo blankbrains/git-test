aa8df7c7375f7fb1efa1ce1bd1a5ecff0de7a59c
blankbrains <990937964@qq.com>
2022-02-26 14:56:42 +0800

    Hot-Fix Commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -6,5 +6,7 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git two");
+        System.out.println("hot-fix Git");
     }
 }
